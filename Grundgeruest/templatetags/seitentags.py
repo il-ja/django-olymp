3058797d0c18e3e5ea2ab2af1fbac7939d6d4c5b
ilja <i-l-j-a@arcor.de>
2018-08-02 20:25:26 +0200

    Templatetags fuer Listen (admin-Link!) eingefuehrt

---
+++ Grundgeruest/templatetags/seitentags.py
@@ -0,0 +1,38 @@
+from django import template
+from Grundgeruest.models import Nutzer
+
+register = template.Library()
+
+@register.inclusion_tag('Grundgeruest/kopfzeile_knopf.html')
+def kopfleiste_knoepfe(user):
+    """ Der tag erwartet von der Funktion ein dict, in dem die Liste der
+    url-text-Paare für die Knöpfe der Kopfleiste steht """
+
+    return {'knoepfe': Nutzer.knoepfe_kopf(user)}
+
+@register.inclusion_tag('Grundgeruest/menueleiste_knopf.html')
+def menueleiste_knoepfe(user):
+    """ gibt ein dict zurück, in dem die Liste der url-text-Paare für die
+    Knöpfe der Menüleiste steht """
+
+    return {'knoepfe': Nutzer.knoepfe_menü(user)}
+
+@register.inclusion_tag('Grundgeruest/listeneintrag.html')
+def listeneintrag(objekt, request, nur_link=False):
+    """ Templatetag wird für eine Verknüpfung zu Objekten, zu denen es
+    eine Detailseite gibt, verwendet; z.B. für einen Punkt innerhalb
+    einer Liste. Falls der request zu einem admininterface-berechtigten
+    gehört, folgt hinter dem normalen Link ein Link zur entsprechenden
+    Seite unter /admin/[...change-view]
+    """
+
+    return {
+        'objekt': objekt,
+        'ob_zeigen': request.user.is_staff,
+        'nur_link': nur_link,
+        'admin_link': '/admin/{app}/{model}/{pk}/change'.format(
+            app=objekt.__class__.__module__.split('.')[0].capitalize(),
+            model=objekt.__class__.__name__.lower(),
+            pk=objekt.pk,
+        ),
+    }
