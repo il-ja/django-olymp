33b6413fb751fd7d5e667d8477b4de8c81623851
ilja <i-l-j-a@arcor.de>
2018-09-26 10:46:44 +0200

    Korrektur zu PR #11 und issue #8 bearbeitet
     - migration hinzugefügt, die im PR #11 falsch war
     - Knöpfe-issue im wesentlichen gelöst:
     - kein Spam-Button und keine Startseite (die redirected zu /olymp/)
     - vorläufiges Impressum hinzugefügt
     - linkus randomus wählt aus einem pool von Objekten per Zufall aus

--- Grundgeruest/views.py
+++ Grundgeruest/views.py
@@ -1,9 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    """ zeigt die Startseite an :) """
-    template_name = 'Grundgeruest/templates/Grundgeruest/startseite.html'
+class IndexView(RedirectView):
+    permanent = False
+    url = "/olymp"
+
+class RandomusView(RedirectView):
+    permanent = False
+    def get_redirect_url(self, *args, **kwargs):
+        from random import random, choice
+        from Wettbewerbe.models import Veranstaltung, WettbewerbKonkret
+        if random() < 0.3:
+            return "/olymp/"
+        if random() < 0.5:
+            return choice(Veranstaltung.objects.all()).get_absolute_url()
+        
+        return choice(WettbewerbKonkret.objects.all()).get_absolute_url()
+
+
--- seite/urls.py
+++ seite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.views.generic import TemplateView
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
@@ -28,10 +29,7 @@ urlpatterns = [
     url(r'^olymp/', include('Wettbewerbe.urls')),
     url(r'^martor/', include('martor.urls')),
     url(r'^kommentare/', include('Kommentare.urls')),
-    url(r'^$',
-        grundgeruest.IndexView.as_view(
-            template_name='Grundgeruest/startseite.html'
-        ),
-        name='startseite',
-    ),
+    url(r'^impressum/$', TemplateView.as_view(template_name="impressum.html"), name='startseite'),
+    url(r'^linkus-randomus/$', grundgeruest.RandomusView.as_view(), name='linkus-randomus'),
+    url(r'^$', grundgeruest.IndexView.as_view(), name='startseite'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
