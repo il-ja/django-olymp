84eb7891511a8fd78b5f6e9308e4d1f1a7243fd7
ilja <i-l-j-a@arcor.de>
2018-12-04 06:14:00 +0100

    versuche requirements zu fixen

--- seite/wsgi.py
+++ seite/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seite.settings")
+if os.path.isfile("./seite/local_settings.py"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seite.local_settings")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seite.settings")
 
 application = get_wsgi_application()
